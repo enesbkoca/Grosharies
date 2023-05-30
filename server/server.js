eab7b653c87a69f8f57d3820de06f60476dcd7f0
Enes <E.B.Koca@student.tudelft.nl>
2023-05-30 07:46:51 +0200

    Reorganizing of structure

--- server/server.js
+++ server/server.js
@@ -1,27 +1,23 @@
 const express = require("express");
 
 const app = express();
+const connectDB = require('./config/db');
 
-const cors = require("cors");
-
-require("dotenv").config({ path: "./config.env"});
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+connectDB();
 
-app.use(express.json());
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.use(require("./routes/record"));
-
-// Get MongoDB driver connection
-const dbo = require("./db/conn");
 
 app.listen(port, () => {
-    // Perform database connection when server starts
-    dbo.connectToServer(function (err) {
-        if (err) console.error(err);
-    });
+    // // Perform database connection when server starts
+    // dbo.connectToServer(function (err) {
+    //     if (err) console.error(err);
+    // });
 
     console.log(`Server is running on port ${port}`);
+
+
 
 });
