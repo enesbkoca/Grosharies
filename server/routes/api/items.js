1df7b9158370727ecc1bedbc24f863fc4a3b305f
berkay <enesbkoca@gmail.com>
2023-06-13 08:27:07 +0200

    Finish implementing api route to delete all items

--- server/routes/api/items.js
+++ server/routes/api/items.js
@@ -51,7 +51,9 @@ router.put('/:id', (req, res) => {
 // @description Delete all items
 // @access Public
 router.delete('/', (req, res) => {
-    GroceryItem.deleteMany()
+    GroceryItem.deleteMany({})
+        .then(item => res.json({msg: "All items deleted successfully"}))
+        .catch(err => res.status(400).json({error: "Unable to delete all items"}))
 })
 
 
@@ -60,7 +62,7 @@ router.delete('/', (req, res) => {
 // @access Public
 router.delete('/:id', (req, res) => {
     GroceryItem.findByIdAndRemove(req.params.id, req.body)
-        .then(book => res.json({msg: "Book entry deleted successfully"}))
+        .then(item => res.json({msg: "Item entry deleted successfully"}))
         .catch(err => res.status(404).json({error: "No such book found"}))
 });
 
